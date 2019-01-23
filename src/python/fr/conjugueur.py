b8b88dbd8a6558daf8f5e55ee11cbdb984cd1286
Adriano Gil <adrianogil.san@gmail.com>
2019-01-23 13:41:08 -0400

    Added passe compose

--- src/python/fr/conjugueur.py
+++ src/python/fr/conjugueur.py
@@ -26,12 +26,26 @@ def print_verbe(pronoun, verb):
     else:
         print(pronoun + " " + verb)
 
+def get_participe(verb):
+    if verb == "avoir":
+        return "eu"
+
+    return verb[:-2] + "é"
+
 def get_verb_radical(pronoun, verb, mode, temps):
     # Regular verbs - Indicatif présent
-    if verb == "avoir":
-        return ""
+    if mode == "indicatif":
+        if temps == "présent":
+            if verb == "avoir":
+                return "%s"
 
-    return verb[:-2]
+            return verb[:-2] + "%s"
+        elif temps == "passé composé":
+            avoir_auxiliar = get_verb_desinence(pronoun, "avoir", "indicatif", "présent")
+
+            return avoir_auxiliar + " " + get_participe(verb) + "%s"
+
+    return ""
 
 def get_verb_desinence(pronoun, verb, mode, temps):
     desinence = {}
@@ -40,7 +54,7 @@ def get_verb_desinence(pronoun, verb, mode, temps):
         if temps == 'présent':
             if verb == 'avoir':
                 desinence['je'] = 'ai'
-                desinence['tu'] = 'a'
+                desinence['tu'] = 'as'
                 desinence['il'] = 'a'
                 desinence['elle'] = 'a'
                 desinence['nous'] = 'avons'
@@ -56,6 +70,8 @@ def get_verb_desinence(pronoun, verb, mode, temps):
                 desinence['vous'] = 'ez'
                 desinence['ils'] = 'ent'
                 desinence['elles'] = 'ent'
+        elif temps == 'passé composé':
+            return ""
 
     return desinence[pronoun.lower()]
 
@@ -63,7 +79,15 @@ def get_verb_desinence(pronoun, verb, mode, temps):
 def conjugueur(pronoun, verb, mode, temps):
     verb_radical = get_verb_radical(pronoun, verb, mode, temps)
     desinence_verbale = get_verb_desinence(pronoun, verb, mode, temps)
-    print_verbe(pronoun, verb_radical + desinence_verbale)
+    print_verbe(pronoun, verb_radical % (desinence_verbale))
 
-for p in pronouns:
-    conjugueur(p, target_verb, "indicatif", "présent")
+modes = ["indicatif"]
+temps = {"indicatif": ["présent", "passé composé"]}
+
+for m in modes:
+    print(m)
+    for t in temps[m]:
+        print(t)
+        for p in pronouns:
+            conjugueur(p, target_verb, m, t)
+        print("")
