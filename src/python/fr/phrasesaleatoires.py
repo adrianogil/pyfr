ea96a442ce3809bb251aca973642db4d45bb7a3d
Adriano Gil <adrianogil.san@gmail.com>
2020-08-01 17:39:43 -0400

    Improve FR grammar using dictionary

--- src/python/fr/phrasesaleatoires.py
+++ src/python/fr/phrasesaleatoires.py
@@ -1,52 +1,51 @@
-from simplegrammar.grammar import SimpleGrammar
+from simplegrammar import SimpleGrammar
 
 
-phrase_aleatoire = SimpleGrammar() \
-.set_text("#phrase_aleatoire#")\
-    .add_tag("phrase_aleatoire", [
+fr_grammar = {
+    "text": ["#phrase_aleatoire#"],
+    "phrase_aleatoire": [
         "#des_que_je_peux#",
         "#presentation_de_quelquun#",
         "#des_nos_jours#",
-    ])\
-    .add_tag("des_que_je_peux", [
-        "#chose_je_fait_tout_les_temps# dès que je peux."
-    ])\
-    .add_tag("chose_je_fait_tout_les_temps", [
+        "#je_me_suis_tout_daccord#"
+    ],
+    "je_me_suis_tout_daccord": ["Je suis tout à fait d'accord avec #chose_que_je_suis_daccord_avec#",
+                                "Je suis entièrement d'accord avec #chose_que_je_suis_daccord_avec#",
+                                "Je suis d'accord avec #chose_que_je_suis_daccord_avec#"],
+    "chose_que_je_suis_daccord_avec": ["la politique dingue du Brèsil"],
+    "des_que_je_peux": ["#chose_je_fait_tout_les_temps# dès que je peux."],
+    "chose_je_fait_tout_les_temps": [
         "Je t'apporte #chose_a_tapporter#",
         "Je vais au cinéma",
         "J'áchete #chose_a_acheter#"
-    ])\
-    .add_tag("chose_a_acheter", [
+    ],
+    "chose_a_acheter": [
         "une voiture",
         "un jouet",
         "un ordinateur"
-    ])\
-    .add_tag("chose_a_tapporter", [
+    ],
+    "chose_a_tapporter": [
         "la bouillie",
         "une fleur",
-    ])\
-    .add_tag("presentation_de_quelquun", [
-        "D'abord #je_suis_mon_nom#."
-    ])\
-    .add_tag("je_suis_mon_nom", [
-        "Je suis #nom_personne#"
-    ])\
-    .add_tag("nom_personne", [
+    ],
+    "presentation_de_quelquun": ["D'abord #je_suis_mon_nom#."],
+    "je_suis_mon_nom": ["Je suis #nom_personne#"],
+    "nom_personne": [
         "Adriano",
         "Albert",
         "Jussara",
         "Eliamara",
         "Roberto",
         "Geovana"
-    ])\
-    .add_tag("des_nos_jours", [
+    ],
+    "des_nos_jours": [
         "Des nos jours, #chose_etrange_des_nos_jours# est devenu un vrai luxe.",
         "Des nos jours, #chose_etrange_des_nos_jours# est devenu un vrai luxe qui n'est pas donné à tout le monde.",
         "Des nos jours, la technologie en matière de téléphonie permet à un appelant d'empêcher l'affichage de son identité et de son numéro de téléphone."
-    ])\
-    .add_tag("chose_etrange_des_nos_jours", [
-        "regarder le ciel bleu et respirer de l'air frais",
-    ])\
+    ],
+    "chose_etrange_des_nos_jours": ["regarder le ciel bleu et respirer de l'air frais"]
+}
 
 
-print(str(phrase_aleatoire))
+phrase_aleatoire = SimpleGrammar()
+print(phrase_aleatoire.parse(fr_grammar))
