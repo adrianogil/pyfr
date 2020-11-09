58144fdd51050f5224fb46da31916154e6c909dd
Adriano Gil <adrianogil.san@gmail.com>
2020-11-08 21:23:01 -0400

    Added more phrases

--- src/python/fr/phrasesaleatoires.py
+++ src/python/fr/phrasesaleatoires.py
@@ -7,7 +7,8 @@ fr_grammar = {
         "#des_que_je_peux#",
         "#presentation_de_quelquun#",
         "#des_nos_jours#",
-        "#je_me_suis_tout_daccord#"
+        "#je_me_suis_tout_daccord#",
+        "#exprimer_desaccord#",
     ],
     "commence_pour_expliquer": [
         "En effet",
@@ -26,7 +27,26 @@ fr_grammar = {
         "Je suis entièrement d'accord avec #chose_que_je_suis_daccord_avec#",
         "Je suis d'accord avec #chose_que_je_suis_daccord_avec#"
     ],
-    "chose_que_je_suis_daccord_avec": ["la politique dingue du Brèsil"],
+    "de_chose_que_je_ne_suis_pas_daccord": [
+        "voter pour Donald Trump"
+    ],
+    "que_chose_que_je_ne_suis_pas_daccord": [
+        "Bolsonaro continue à faire ses mauvaises choses"
+    ],
+    "exprimer_desaccord": [
+        "C'est inacceptable",
+        "C'est ridicule",
+        "Je suis contre l'idée de #de_chose_que_je_ne_suis_pas_daccord#",
+        "On a tort de penser que #que_chose_que_je_ne_suis_pas_daccord#",
+        "On a tort de croire que #que_chose_que_je_ne_suis_pas_daccord#",
+        "C'est tout à fait faux de dire que #que_chose_que_je_ne_suis_pas_daccord#",
+        "C'est absolument faux de dire que #que_chose_que_je_ne_suis_pas_daccord#",
+        "C'est inexact d'affirmer que #que_chose_que_je_ne_suis_pas_daccord#"
+    ],
+    "chose_que_je_suis_daccord_avec": [
+        "la politique dingue du Brèsil",
+        "sauvegarder l'Amazonie d'intérêts étrangeres"
+    ],
     "des_que_je_peux": ["#chose_je_fait_tout_les_temps# dès que je peux."],
     "chose_je_fait_tout_les_temps": [
         "Je t'apporte #chose_a_tapporter#",
@@ -43,14 +63,19 @@ fr_grammar = {
         "une fleur",
     ],
     "presentation_de_quelquun": ["D'abord #je_suis_mon_nom#."],
-    "je_suis_mon_nom": ["Je suis #nom_personne#"],
+    "je_suis_mon_nom": [
+        "Je suis #nom_personne#",
+        "Je m'appele #nom_personne#"
+    ],
     "nom_personne": [
         "Adriano",
         "Albert",
         "Jussara",
         "Eliamara",
         "Roberto",
-        "Geovana"
+        "Geovana",
+        "Marie",
+        "Sophie"
     ],
     "des_nos_jours": [
         "Des nos jours, #chose_etrange_des_nos_jours# est devenu un vrai luxe.",
